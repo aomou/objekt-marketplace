47ccaee0d2970f61f53489a47af1a0e4203777d4
aomou <100065388+aomou@users.noreply.github.com>
2025-09-12 15:38:25 +0800

    Adjust objekt views 2

--- apps/objekt/views.py
+++ apps/objekt/views.py
@@ -5,7 +5,11 @@ from .models import ObjektCard  # 從 models.py 匯入資料表
 # 接收 request → 取得資料 models → 把資料丟給 template → render 渲染產生 HTML → 回傳給瀏覽器
 
 def all_card_list(request):
-    # 取出所有資料(前50) 先查詢好 FK
-    qs = ObjektCard.objects.select_related('objekt_type',).all()[:50]
-    return render(request, template_name='objekt/list.html', context={'cards': qs})
+    # 取得要顯示的資料
+    # 取出所有資料(前50) 先查詢好 FK ???
+    qs = ObjektCard.objects.select_related('objekt_type', 'objekt_type__artist').all()[:50]
+    context = {
+        'cards': qs
+        }
+    return render(request, template_name='objekt/list.html', context=context)
 
