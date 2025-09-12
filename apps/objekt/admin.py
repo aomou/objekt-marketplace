7703c54ee9c9880960951ae22c81cfc58ce5b56c
aomou <100065388+aomou@users.noreply.github.com>
2025-09-12 15:19:53 +0800

    Fix admin class

--- apps/objekt/admin.py
+++ apps/objekt/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Artist, Season, Class, Member, Collection, ObjektType, ObjektCard
 
 # Register models 把定義好的資料表 models 掛到 admin 後台管理系統  
-admin.site.register([Artist, Season, Class, Member, ObjektCard])
+admin.site.register([Artist, Season, Class, Member, ObjektCard, ObjektType])
 
 # 設定後台顯示的欄位
 class CollectionAdmin(admin.ModelAdmin):
