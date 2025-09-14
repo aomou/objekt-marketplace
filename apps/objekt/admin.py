b99962a639630e9febee263ccfa9f98b04958ecb
aomou <100065388+aomou@users.noreply.github.com>
2025-09-14 22:53:54 +0800

    Add admin display column

--- apps/objekt/admin.py
+++ apps/objekt/admin.py
@@ -2,10 +2,20 @@ from django.contrib import admin
 from .models import Artist, Season, Class, Member, Collection, ObjektType, ObjektCard
 
 # Register models 把定義好的資料表 models 掛到 admin 後台管理系統  
-admin.site.register([Artist, Season, Class, Member, ObjektCard, ObjektType])
+admin.site.register([Artist, Class, ObjektCard, ObjektType])
 
 # 設定後台顯示的欄位
-class CollectionAdmin(admin.ModelAdmin):
-    list_display = ('name', 'physical', )
+# Member
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('name', 'artist', 'memberCode')
+admin.site.register(Member, MemberAdmin)
 
+# Collection
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'physical')
 admin.site.register(Collection, CollectionAdmin)
+
+# Season
+class SeasonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'artist')
+admin.site.register(Season, SeasonAdmin)
