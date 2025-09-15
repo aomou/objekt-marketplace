08e108e885b97ff0119cedd535aad25910bfb5b1
aomou <100065388+aomou@users.noreply.github.com>
2025-09-15 22:43:25 +0800

    Fix home url

--- config/urls.py
+++ config/urls.py
@@ -16,14 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView  # login logout
+from apps.objekt.views import filtered_objekt_type  # import objekt view
 
 urlpatterns = [
+    path('', filtered_objekt_type, name='home'),
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='card_list'), name='logout'),
     path('objekt/', include('apps.objekt.urls')),
     path('accounts/', include('apps.accounts.urls')),
     #path('marketplace/', include('apps.marketplace.urls')),
-
 ]
