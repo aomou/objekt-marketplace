c6d1532ba36994848202601a12e3a145e892c0e5
aomou <100065388+aomou@users.noreply.github.com>
2025-09-16 10:40:56 +0800

    Add profile edit area to accounts info

---
+++ apps/accounts/forms.py
@@ -0,0 +1,28 @@
+# 定義 update User, Profile 的填寫欄位
+
+from django import forms
+from .models import User, UserProfile
+from django.contrib.auth.models import User  # 修正 import
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+        widgets = {
+            "username": forms.TextInput(attrs={"placeholder": "username"}),
+            "email": forms.EmailInput(attrs={"placeholder": "email@example.com"}),
+        }
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['wallet_address', 'contact_info']
+        widgets = {
+            "wallet_address": forms.TextInput(attrs={"placeholder": "0x123456..."}),
+            "contact_info": forms.Textarea(attrs={
+                "rows": 5, 
+                "placeholder": "Discord username, X id, ..."
+                }),
+        }
+    
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
-from .models import UserProfile
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import UserProfile
+from .forms import UserForm, ProfileForm
 
 # views
 
 @login_required  # 如果沒登入就跳轉到 /accounts/login/ 登入頁面
 def account_info(request):
-    # 內建的使用者資料
-    user = request.user  # user 會自動傳入不用放在 context
+    # 內建的使用者資料   user 會自動傳入不用放在 context
+
     # 自訂的 user profile
     # 如果沒有 Profile 就先 create
     user_profile, profile_created = UserProfile.objects.get_or_create(
@@ -22,10 +24,30 @@ def account_info(request):
     # example 
     # owned_list = ObjektList.objects.filter(owner=request.user).order_by('created_at')
 
+    # 填寫 / 修改 Profile
+    # POST
+    if request.method == 'POST': 
+        user_form = UserForm(request.POST, instance=request.user, prefix='user')
+        profile_form = ProfileForm(request.POST, instance=user_profile, prefix='profile')
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'User info and profile updated!')
+            return redirect('account_info')  # 在同一頁的話也要 redirect 嗎
+        else:
+            messages.error(request, 'Wrong input!')
+    # GET
+    else: 
+        user_form = UserForm(instance=request.user, prefix='user')
+        profile_form = ProfileForm(instance=user_profile, prefix='profile')
+ 
     context = {
         'username': request.user.username,
         'user_profile': user_profile,
         'profile_created': profile_created,
+        'user_form': user_form,
+        'profile_form': profile_form,
         }
 
     return render(request, template_name='accounts/info.html', context=context)
