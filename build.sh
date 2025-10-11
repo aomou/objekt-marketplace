741d9f9a361e038683baa6944d34cc8fe8bae197
aomou <100065388+aomou@users.noreply.github.com>
2025-10-11 18:14:59 +0800

    Auto-load fixture when deploy

--- build.sh
+++ build.sh
@@ -2,7 +2,16 @@
 # exit on error
 set -o errexit
 
+# 安裝 pack
 pip install -r requirements.txt
 
+# 收集靜態檔案
 python manage.py collectstatic --no-input
+
+# 資料庫 migrate
 python manage.py migrate
+
+# 匯入 fixture（放在各 app 的 fixtures 資料夾）
+python manage.py loaddata apps/objekt/fixtures/objekt_fixtures.json || true
+python manage.py loaddata apps/accounts/fixtures/accounts_fixtures.json || true
+python manage.py loaddata apps/marketplace/fixtures/marketplace_fixtures.json || true
