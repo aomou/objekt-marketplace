0932c095e43854bc382ecf4624b64f5255ae9844
aomou <100065388+aomou@users.noreply.github.com>
2025-11-25 18:16:24 +0800

    Reload fixture for new db

--- build.sh
+++ build.sh
@@ -13,10 +13,10 @@ python manage.py migrate
 
 # 首次部署時要用
 # 建立 admin / superuser 
-# python manage.py createsuperuser --noinput || true   
+python manage.py createsuperuser --noinput || true   
 
 # 匯入 fixture（放在各 app 的 fixtures 資料夾）
-# python manage.py loaddata apps/objekt/fixtures/objekt_fixtures.json || true
-# python manage.py loaddata apps/accounts/fixtures/accounts_fixtures.json || true
-# python manage.py loaddata apps/marketplace/fixtures/marketplace_fixtures.json || true
+python manage.py loaddata apps/objekt/fixtures/objekt_fixtures.json || true
+python manage.py loaddata apps/accounts/fixtures/accounts_fixtures.json || true
+python manage.py loaddata apps/marketplace/fixtures/marketplace_fixtures.json || true
 
